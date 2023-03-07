b98f493c0274d0718270f02cf9bd1cd2e3e5a047
peyton-spencer <pas1234567@gmail.com>
2023-03-07 14:14:09 -0700

    main.rs: access the deployed GOERLI counter contract

--- app/src/main.rs
+++ app/src/main.rs
@@ -10,10 +10,15 @@ async fn main() -> Result<()> {
     let provider = GOERLI.provider();
     let provider = Arc::new(provider);
 
-    let address = "0x0000000000000000000000000000000000000000".parse::<Address>()?;
+    // deployed counter address
+    let address = "0x2AB57b13a53A5aB77F9FA93519DB560B6293339C".parse::<Address>()?;
 
     let contract = Counter::new(address, provider);
     let blk = contract.client().get_block_number().await?;
+    // contract.set_number(ethers::types::U256::from(867));
+    let num = contract.number().call().await?;
+
     println!("Hello, world! {}", blk);
+    println!("contract: {}; counter: {}", contract.address(), num);
     Ok(())
 }
